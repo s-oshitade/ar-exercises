a816be2e78565c5a6b0787e64600d026b5ecca1a
Seun Oshitade <seun.oshitade@gmail.com>
2022-06-29 15:45:29 +0000

    Create 3 stores and assign them to variables

--- exercises/exercise_1.rb
+++ exercises/exercise_1.rb
@@ -5,10 +5,10 @@ puts "----------"
 
 # Your code goes below here ...
 
-store1 = Store.create(name: "Burnaby", annual_revenue: 300000, mens_apparel: true, womens_apparel: true)
+burnaby = Store.create(name: "Burnaby", annual_revenue: 300000, mens_apparel: true, womens_apparel: true)
 
-store2 = Store.create(name: "Richmond", annual_revenue: 1260000, mens_apparel: false, womens_apparel: true)
+richmond = Store.create(name: "Richmond", annual_revenue: 1260000, mens_apparel: false, womens_apparel: true)
 
-store3 = Store.create(name: "Gastown", annual_revenue: 190000, mens_apparel: true, womens_apparel: false)
+gastown = Store.create(name: "Gastown", annual_revenue: 190000, mens_apparel: true, womens_apparel: false)
 
 puts "The number of stores is: #{Store.count}" 
