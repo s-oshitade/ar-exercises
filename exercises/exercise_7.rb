74b9a430ae5ddfb8803a8c3cd7d0c5fecdfdd732
Seun Oshitade <seun.oshitade@gmail.com>
2022-06-30 09:21:18 +0000

    Implement validation for both models

--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -10,3 +10,16 @@ puts "Exercise 7"
 puts "----------"
 
 # Your code goes here ...
+
+puts "Please enter new store name"
+print"> "
+
+new_store_name = $stdin.gets.chomp
+
+@new_store = Store.create(name: new_store_name)
+
+if @new_store.errors
+  @new_store.errors.full_messages.each do |error|
+    pp error
+  end
+end
