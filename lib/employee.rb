d15d6aa372c6a37fff0a1480995c9650afa4a634
Seun Oshitade <seun.oshitade@gmail.com>
2022-06-30 21:12:10 +0000

    Fix bug

--- lib/employee.rb
+++ lib/employee.rb
@@ -4,5 +4,5 @@ class Employee < ActiveRecord::Base
   validates :first_name, presence: true
   validates :last_name, presence: true
   validates :hourly_rate, numericality: {only_integer: true, greater_than_or_equal_to: 40, less_than_or_equal_to: 200} 
-  validates :store_id, presence, true
+  validates :store_id, presence: true
 end
